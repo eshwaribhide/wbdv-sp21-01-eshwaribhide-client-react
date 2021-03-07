da7b1d44deb05c26ae34ccfd6e92575824c49cdf
Eshwari Bhide <ebhide99@gmail.com>
2021-03-06 20:49:58 -0800

    editing dir structure

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import CourseManager from "./components/course-manager";
+import CourseManager from "./components/course-manager/course-manager";
 import CourseEditor from "./components/course-editor/course-editor";
 import {BrowserRouter, Route} from "react-router-dom";
 import Home from "./components/home"
