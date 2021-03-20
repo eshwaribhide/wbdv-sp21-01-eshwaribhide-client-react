fba7c6733bcfc3b533a457e3ab02c8a5bd96533b
Eshwari Bhide <ebhide99@gmail.com>
2021-03-20 13:34:21 -0700

    changing position of red plus icon

--- src/components/course-manager/course-manager.js
+++ src/components/course-manager/course-manager.js
@@ -96,7 +96,7 @@ class CourseManager extends React.Component {
             <br/>
         </div>
 
-        <span class=" fa-stack fa-2x" style={{position: "absolute", bottom: "10%", right:"5%"}}>
+        <span class=" fa-stack fa-2x" style={{position: "absolute", bottom: "5%", right:"1%"}}>
           <i class="fa fa-circle fa-stack-2x" style={{color: "Red"}} onClick={this.addCourse}></i>
           <i class="fa fa-plus fa-stack-1x fa-inverse" onClick={this.addCourse}></i>
         </span>
