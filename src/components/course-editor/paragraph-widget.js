0c1f486e20c637306d04cfee0b3a49f3bc7c49b1
Eshwari Bhide <ebhide99@gmail.com>
2021-03-20 01:45:47 -0700

    adding paragraph widget

--- src/components/course-editor/paragraph-widget.js
+++ src/components/course-editor/paragraph-widget.js
@@ -1,23 +1,35 @@
 import React, {useState, useEffect} from 'react'
 
-const ParagraphWidget = ({widget, setWidget, editing}) => {
+const ParagraphWidget = ({widget, updateWidget, deleteWidget}) => {
+const [editing, setEditing] = useState(false)
+ const [cachedItem, setCachedItem] = useState(widget)
     return (
         <div>
             {
                 editing &&
+                <>
                 <textarea
-                    onChange={(e) => setWidget({...widget, text: e.target.value})}
-                    value={widget.text}
+                    onChange={(e) => setCachedItem({...cachedItem, text: e.target.value})}
+                    value={cachedItem.text}
                     className="form-control"></textarea>
+                <i onClick={() => {setEditing(false); updateWidget(cachedItem.id, cachedItem)}} className="fas fa-check float-right"></i>
+                            <i onClick={() => {setEditing(false); deleteWidget(widget)}} className="fas fa-times float-right"></i>
+                </>
             }
             {
                 !editing &&
+                <>
                     <p>
-                        {widget.text}
+                        {cachedItem.text}
                     </p>
+                    <i onClick={() => setEditing(true)} className="fas fa-cog float-right"></i>
+                </>
             }
         </div>
     )
 }
 
 export default ParagraphWidget
+
+
+
