1f5d7e7817b074fbe3ea1df10d373ef5dd1d40b8
Eshwari Bhide <ebhide99@gmail.com>
2021-04-07 23:07:37 -0700

    moving stuff into service

--- src/components/quizzes/quiz.js
+++ src/components/quizzes/quiz.js
@@ -1,14 +1,13 @@
 import React, {useState, useEffect} from "react";
 import {useParams} from 'react-router-dom'
 import Question from "./questions/question";
+import questionService from "../../services/question-service";
 
 const Quiz = () => {
     const {courseId, quizId} = useParams();
     const [questions, setQuestions] = useState([]);
     useEffect(() => {
-        // TODO: move this to a service file
-        fetch(`http://localhost:3000/api/quizzes/${quizId}/questions`)
-            .then(response => response.json())
+       questionService.findAllQuestions(quizId)
             .then(questions => setQuestions(questions))
         },[])
 
