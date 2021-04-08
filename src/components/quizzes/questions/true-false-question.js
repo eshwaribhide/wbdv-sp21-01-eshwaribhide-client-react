7b961ac99f752e6b9f80392dfebaacb8a74ec382
Eshwari Bhide <ebhide99@gmail.com>
2021-04-07 20:58:40 -0700

    adding true false component functionality

--- src/components/quizzes/questions/true-false-question.js
+++ src/components/quizzes/questions/true-false-question.js
@@ -2,38 +2,131 @@ import React, {useState} from "react";
 
 const TrueFalseQuestion = ({question}) => {
     const [answer, setAnswer] = useState(null)
-    const [graded, setGraded] = useState(false)
+    const [isAnswerCorrect, setIsAnswerCorrect] = useState(null)
     return (
                 <>
                 <h4>
                     {question.question}
-                {
-                    graded && JSON.stringify(answer) == question.correct &&
-                    <i className="fas fa-check"></i>
-                }
-                {
-                    graded && JSON.stringify(answer) != question.correct &&
-                    <i className="fas fa-times"></i>
-                }
+                    {
+                                        isAnswerCorrect!= null && isAnswerCorrect &&
+                                        <i className="fas fa-check" style={{color:"green"}}></i>
+                                    }
+                                    {
+                                         isAnswerCorrect!= null && !isAnswerCorrect &&
+                                        <i className="fas fa-times" style={{color:"red"}}></i>
+                                    }
+
                 </h4>
                 <div class="col-3">
                 <ul className="list-group">
-                    <li className="list-group-item">
-                    <label><input
-                                    type="radio"
-                                    onClick={() => setAnswer(true)}
-                                    name={question._id}/>True</label>
-                     </li>
-                     <li className="list-group-item">
-                    <label><input
-                        type="radio"
-                        onClick={() => setAnswer(false)}
-                        name={question._id}/>False</label>
-                    </li>
+                {isAnswerCorrect == null &&
+                <>
+                <li className="list-group-item">
+                <label><input
+                                type="radio"
+                                onClick={() => setAnswer(true)}
+                                name={question._id}/>True</label>
+                 </li>
+                 <li className="list-group-item">
+                                  <label><input
+                                                  type="radio"
+                                                  onClick={() => setAnswer(false)}
+                                                  name={question._id}/>False</label>
+                                   </li>
+                                   </>}
+
+                {isAnswerCorrect != null && isAnswerCorrect && answer &&
+                <>
+                <li className="list-group-item list-group-item-success">
+                <label><input
+                                                type="radio"
+                                                checked
+                                                onClick={() => setAnswer(true)}
+                                                name={question._id}/>True
+                                                <i className="fas fa-check" style={{color:"green"}}></i>
+                                                </label>
+                                 </li>
+                <li className="list-group-item">
+                                                  <label><input
+                                                                  type="radio"
+                                                                  onClick={() => setAnswer(false)}
+                                                                  name={question._id}/>False</label>
+                                                   </li>                </> }
+
+                {isAnswerCorrect != null && isAnswerCorrect && !answer &&
+                <>
+                <li className="list-group-item">
+                                <label><input
+                                                type="radio"
+                                                onClick={() => setAnswer(true)}
+                                                name={question._id}/>True</label>
+
+                                 </li>
+                <li className="list-group-item list-group-item-success">
+                <label><input
+                                type="radio"
+                                checked
+                                onClick={() => setAnswer(false)}
+                                name={question._id}/>False
+                                <i className="fas fa-check" style={{color:"green"}}></i>
+                                </label>
+
+
+                 </li>
+                 </>}
+
+
+
+
+                 {isAnswerCorrect != null && !isAnswerCorrect && answer &&
+                 <>
+                 <li className="list-group-item list-group-item-success">
+                 <label><input
+                                                 type="radio"
+                                                 checked
+                                                 onClick={() => setAnswer(true)}
+
+                                                 name={question._id}/>True
+                                                 <i className="fas fa-check" style={{color:"green"}}></i>
+                                                 </label>
+
+                                  </li>
+                 <li className="list-group-item list-group-item-danger">
+                                                   <label><input
+                                                                   type="radio"
+                                                                   onClick={() => setAnswer(false)}
+                                                                   name={question._id}/>False
+                                                                   <i className="fas fa-times" style={{color:"red"}}></i>
+                                                                   </label>
+
+                                                    </li>                </> }
+
+                 {isAnswerCorrect != null && !isAnswerCorrect && !answer &&
+                 <>
+                 <li className="list-group-item list-group-item-danger">
+                                 <label><input
+                                                 type="radio"
+                                                 onClick={() => setAnswer(true)}
+                                                 name={question._id}/>True
+                                                 <i className="fas fa-times" style={{color:"red"}}></i>
+                                                 </label>
+                                  </li>
+                 <li className="list-group-item list-group-item-success">
+                 <label><input
+                                 type="radio"
+                                 checked
+                                 onClick={() => setAnswer(false)}
+                                 name={question._id}/>False
+                                 <i className="fas fa-check" style={{color:"green"}}></i>
+                                 </label>
+
+                  </li>
+                  </>}
+
                 </ul>
                 </div>
                 <br/>
-            <button onClick={() => setGraded(true)} style={{backgroundColor: "green", borderRadius: "5px"}}>
+            <button onClick={() => setIsAnswerCorrect(JSON.stringify(answer) == question.correct)} style={{backgroundColor: "green", borderRadius: "5px"}}>
               Grade
             </button>
             <br/>
@@ -45,3 +138,4 @@ const TrueFalseQuestion = ({question}) => {
 }
 
 export default TrueFalseQuestion;
+
