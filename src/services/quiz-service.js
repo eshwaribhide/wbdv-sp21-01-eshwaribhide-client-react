645e20ee45aad03a22576b0dea1fd8b509de0745
Eshwari Bhide <ebhide99@gmail.com>
2021-04-16 16:47:46 -0700

    adding quiz service submit

--- src/services/quiz-service.js
+++ src/services/quiz-service.js
@@ -2,4 +2,15 @@ export const findAllQuizzes = (quizId) =>
     fetch("http://localhost:3000/api/quizzes")
                 .then(response => response.json())
 
-export default {findAllQuizzes}
+export const submitQuiz = (quizId, questions) => {
+ fetch(`http://localhost:3000/api/quizzes/${quizId}/attempts`, {
+   method: 'POST',
+   body: JSON.stringify(questions),
+   headers: {
+     'content-type': 'application/json'
+   }
+ }).then(response => response.json())
+   .then(result => console.log('result', result))
+}
+
+export default {findAllQuizzes, submitQuiz}
